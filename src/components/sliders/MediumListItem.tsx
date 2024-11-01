cda9ea3fd54d7b52921bb8bd82349ba9db1e8b64
seo0o519 <seoyoung344@naver.com>
2024-11-01 21:49:27 +0900

    design #30 - 슬라이더 아이템 css 수정

--- src/components/sliders/MediumListItem.tsx
+++ src/components/sliders/MediumListItem.tsx
@@ -1,14 +1,13 @@
 "use client";
 import React from "react";
-import Img from "./hotel.png";
 
 
 export default function MediumListItem(){
     return (
-        <div className="w-[154px] h-[178px] ">
-            <img src={Img.src} className="h-[110px] rounded-[4px] w-full" />
-            <div className="w-full font-semibold text-[16px] pt-[8px]">강릉 모던 프라이빗 <br/>공간 | 휴휴가</div>
-            <div className="w-full f-[16px] text-[12px] text-gray-400 pt-[4px]">강릉시 황리단 민속마을</div>
+        <div className="w-[154px]">
+            <img src={"/hotelImage.png"} className="w-[154px] h-[110px] rounded-[4px]" />
+            <div className="w-[154px] font-semibold text-[16px] pt-[8px]">강릉 모던 프라이빗 공간 | 휴휴가</div>
+            <div className="w-full text-[12px] text-gray-700 pt-[4px]">강릉시 황리단 민속마을</div>
         </div>
     );
 }
