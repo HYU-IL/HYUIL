cd6e6f6a69578dc00d92b3d95f5cb948ce93f72e
seo0o519 <seoyoung344@naver.com>
2024-11-07 21:40:04 +0900

    fix #30 - 가로 슬라이더에서 생기는 오류 수정

--- src/components/sliders/MediumListItem.tsx
+++ src/components/sliders/MediumListItem.tsx
@@ -8,10 +8,18 @@ interface MediumListItemProps {
 
 export default function MediumListItem({label, imgurl, addr} : MediumListItemProps){
     return (
-        <div className="w-[154px]">
-            <img src={imgurl} className="w-[154px] h-[110px] rounded-[4px]" />
-            <div className="w-[154px] pt-[8px] font-subtitle2">{label}</div>
-            <div className="w-[154px] text-caption2 text-gray-700 text-opacity-60 pt-[4px]">{addr}</div>
+        <div className="w-[154px] h-fit">
+            <div className="w-[154px] h-[110px] mb-[8px]">
+                <img src={imgurl} className="w-full h-full rounded-[4px]"/>
+            </div>
+                <div className="flex flex-col">
+                <span className="text-[16px] block leading-[20px] whitespace-pre-wrap mb-[4px]">
+                    {label && label}
+                </span>
+                <span className="h-fit text-[12px] text-[#999999] whitespace-pre-wrap">
+                    {addr && addr}
+                </span>
+            </div>
         </div>
     );
 }
--- src/components/sliders/SmallListItem.tsx
+++ src/components/sliders/SmallListItem.tsx
@@ -9,8 +9,10 @@ interface SmallListItemProps {
 export default function SmallListItem({label, imgurl} : SmallListItemProps){
     return (
         <div className="w-[140px]">
-            <img src={imgurl} className="w-[140px] h-[105px] rounded-md" />
-            <div className="w-[140px] pt-[8px] text-subtitle2">{label}</div>
+            <div className="w-[140px] h-[105px] mb-[8px]">
+              <img src={imgurl} className="w-[140px] h-[105px] rounded-[4px] " />
+            </div>
+            <span className="w-[140px] pt-[8px] text-subtitle2 whitespace-pre-wrap">{label}</span>
         </div>
     );
 }
