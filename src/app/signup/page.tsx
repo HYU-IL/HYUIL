21bb1b2f50dc3c8d59f99b525c0e57d772d3a78e
hummingbbird <pop9814@kookmin.ac.kr>
2024-11-11 02:16:34 +0900

    feat #28 - steps 페이지 UI 구현(stepper bar 포함)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,39 +6,125 @@ import PAPERICON from "../../../public/assets/icons/paperplaneicon.svg";
 import MOUNTAIN from "../../../public/assets/images/step_mountain.svg";
 import OCEAN from "../../../public/assets/images/step_ocean.svg";
 import DOWN from "../../../public/assets/icons/downicon.svg";
+import CAMPING from "../../../public/assets/images/step_camping.svg";
+import RESORT from "../../../public/assets/images/step_resort.svg";
+import LEISURE from "../../../public/assets/images/step_leisure.svg";
+import FESTIVAL from "../../../public/assets/images/step_festival.svg";
+import PEOPLE1 from "../../../public/assets/images/step_1or2_on.svg";
+import PEOPLE2 from "../../../public/assets/images/step_3or4_on.svg";
+import PEOPLE3 from "../../../public/assets/images/step_morethan5_on.svg";
+import BYCAR from "../../../public/assets/images/step_car.svg";
+import BYROAD from "../../../public/assets/images/step_road.svg";
 
 export default function SignupPage() {
   const whatStep = useRef(1);
+  const [isFinished, setIsFinished] = useState(false);
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex flex-col w-full pb-[80px]">
       <div className="w-full h-[48px] items-center justify-center flex border-b">
         <Image src={LOGO} alt="logo" className="w-[76px]" />
       </div>
 
       {/* stepper div */}
-      <div className="flex w-full items-center justify-center text-[12px] leading-3 tracking-tight text-gray-700 pt-3">
-        {whatStep.current}/3
+      <div className="flex flex-col w-full items-center justify-center">
+        <div className="flex flex-row w-full h-[3px]">
+          <div
+            className={`w-[${whatStep.current*33}%] bg-primary-core items-start`}
+          />
+          <div
+            className={`w-[${100 - (whatStep.current*33)}%] bg-gray-300 items-start`}
+          />
+        </div>
+        <span className="text-[12px] leading-3 tracking-tight text-gray-700 pt-3 items-center">
+          {whatStep.current}/3
+        </span>
       </div>
+
       {/* step1 div */}
-      <div className="flex flex-col font-Pretendard text-[20px] leading-6 tracking-tight font-semibold text-primary-800 mx-[16px] border border-red-500 mt-[50px]">
+      <div className="flex flex-col font-Pretendard text-[20px] leading-6 tracking-tight font-semibold text-primary-800 mx-[16px] mt-[50px]">
         <span className="ml-3">평소, 가장 가고 싶은</span>
         <span className="ml-3">워케이션 한 곳을 선택해주세요</span>
         <div className="flex flex-row gap-1 text-caption1 text-sub-core pt-[8px] pb-[24px] ml-3">
           <Image src={PAPERICON} alt="icon" />
           선택한 유형을 기반으로 추천해드려요
         </div>
-        <div className="flex flex-row justify-center rounded-xl shadow-lg py-[8px] gap-[8px] w-full h-[255px] border">
+        <div className="flex flex-row justify-center rounded-xl shadow-lg py-[8px] gap-[8px] w-full h-[255px]">
           <Image src={MOUNTAIN} alt="moutain" />
           <Image src={OCEAN} alt="ocean" />
         </div>
       </div>
 
       {/* 내려가요 div */}
-      <div className="flex flex-col w-full items-center text-body2 text-gray-900 pt-5 border border-blue-500">
+      <div className="flex flex-col w-full items-center text-body2 text-gray-900 pt-5 ">
         <span>선택하면 아래로</span>
         <span>내려가요</span>
-        <Image src={DOWN} alt="down" className="pt-2"/>
+        <Image src={DOWN} alt="down" className="pt-2" />
       </div>
+
+      {/* step2 div */}
+      <div className="flex flex-col h-fit font-Pretendard text-[20px] leading-6 tracking-tight font-semibold text-primary-800 mx-[16px] mt-[50px] mb-4">
+        <span className="ml-3">평소, 어디서 편하게</span>
+        <span className="ml-3">휴일을 보내고 싶으신가요?</span>
+        <div className="flex flex-row gap-1 text-caption1 text-sub-core pt-[8px] pb-[24px] ml-3">
+          <Image src={PAPERICON} alt="icon" />
+          2곳을 선택하면 휴일이 기억해서 추천해드려요
+        </div>
+        <div className="flex flex-col items-center justify-center rounded-xl shadow-lg py-[8px] gap-[8px] w-full h-[472px] border">
+          <div className="flex flex-row gap-[8px]">
+            <Image src={CAMPING} alt="camping" />
+            <Image src={RESORT} alt="resort" />
+          </div>
+          <div className="flex flex-row gap-[8px]">
+            <Image src={LEISURE} alt="leisure" />
+            <Image src={FESTIVAL} alt="festival" />
+          </div>
+        </div>
+      </div>
+
+      {/* step3 div */}
+      <div className="flex flex-col h-fit font-Pretendard text-[20px] leading-6 tracking-tight font-semibold text-primary-800 mx-[16px] mt-[50px] mb-4">
+        <span className="ml-3">편한 휴일을 함께</span>
+        <span className="ml-3">보낼 지인은 몇 명인가요?</span>
+        <div className="flex flex-row items-center h-fit gap-1 text-caption1 text-sub-core mt-[8px] pb-[20px] ml-3">
+          <Image src={PAPERICON} alt="icon" />몇 명과 떠날지 하나만 선택해주세요
+        </div>
+        <div className="flex flex-col items-center justify-center w-full">
+          <Image src={PEOPLE1} alt="people" />
+          <Image src={PEOPLE2} alt="people" />
+          <Image src={PEOPLE3} alt="people" />
+        </div>
+      </div>
+
+      {/* 내려가요 div */}
+      <div className="flex flex-col w-full items-center text-body2 text-gray-900 pt-5">
+        <span>선택하면 아래로</span>
+        <span>내려가요</span>
+        <Image src={DOWN} alt="down" className="pt-2" />
+      </div>
+
+      {/* step4(이동방식) div*/}
+      <div className="flex flex-col h-fit font-Pretendard text-[20px] leading-6 tracking-tight font-semibold text-primary-800 mx-[16px] mt-[50px] mb-4">
+        <span className="ml-3">차량과 도보 중</span>
+        <span className="ml-3">어떻게 떠나실 건가요?</span>
+        <div className="flex flex-row items-center h-fit gap-1 text-caption1 text-sub-core mt-[8px] ml-3">
+          <Image src={PAPERICON} alt="icon" />
+          여행 방법에 따라 추천해드려요
+        </div>
+        <div className="flex flex-col items-center justify-center w-full">
+          <Image src={BYCAR} alt="" />
+          <Image src={BYROAD} alt="" />
+        </div>
+      </div>
+
+      <button
+        className={`bg-primary-core text-white text-subtitle1 rounded-lg mx-[28px] py-3 ${
+          isFinished
+            ? ``
+            : `bg-opacity-80 backdrop-filter backdrop-brightness-125`
+        }`}
+      >
+        확인
+      </button>
     </div>
   );
 }
