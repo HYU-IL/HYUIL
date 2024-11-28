f49a5d3ce0a46041778c4236a1fdf4f7cc6ff2f3
hummingbbird <pop9814@kookmin.ac.kr>
2024-11-28 14:39:35 +0900

    feat #44 - LocationBox에 telephone 추가

--- src/app/detail/_component/locationBox.tsx
+++ src/app/detail/_component/locationBox.tsx
@@ -4,9 +4,10 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 import { useParams } from "next/navigation";
 interface LocationBoxProps {
   adr?: string;
+  telephone?: string;
 }
 
-function LocationBox({ adr }: LocationBoxProps) {
+function LocationBox({ adr, telephone }: LocationBoxProps) {
   const params = useParams() as { id: string };
 
   const copyToClipboard = async (text: string) => {
@@ -19,7 +20,7 @@ function LocationBox({ adr }: LocationBoxProps) {
     copyToClipboard(adr || "");
   }
   return (
-    <div className="w-full h-[48%] flex flex-col items-start justify-center p-[16px]">
+    <div className="w-full h-[48%] flex flex-col items-start justify-center p-[16px] pb-8">
       <span className="text-subtitle1 font-bold">위치 및 교통</span>
       <div className="w-full h-[170px] mt-4 rounded-lg bg-gray-200" />
       <div className="flex gap-2  text-caption2 text-[#8f8f8f] py-2">
@@ -35,6 +36,9 @@ function LocationBox({ adr }: LocationBoxProps) {
         <button className="bg-white text-caption3 border border-gray-700 text-gray-700 rounded-[4px] w-[155px] py-1">
           길 안내
         </button>
+      </div>
+      <div className="w-full items-start text-gray-600 text-body2 font-medium pt-2">
+        전화번호: {telephone}
       </div>
       {/* 기타 정보 안내 div */}
       <div></div>
