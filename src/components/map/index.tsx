9263dee471702bfff38bffa18c368a300d67312f
AnCha <tory912@gmail.com>
2024-12-01 16:07:38 +0900

    move #50 - chip아이콘 위치 변경

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,7 +1,7 @@
-import ChipHeart from "../../../public/assets/icons/ChipHeart.svg";
-import ChipPlace from "../../../public/assets/icons/ChipPlace.svg";
-import ChipBalloon from "../../../public/assets/icons/ChipBalloon.svg";
-import ChipCoffee from "../../../public/assets/icons/ChipCoffee.svg";
-import ChipFood from "../../../public/assets/icons/ChipFood.svg";
+import ChipHeart from "../../../public/assets/icons/chip/ChipHeart.svg";
+import ChipPlace from "../../../public/assets/icons/chip/ChipPlace.svg";
+import ChipBalloon from "../../../public/assets/icons/chip/ChipBalloon.svg";
+import ChipCoffee from "../../../public/assets/icons/chip/ChipCoffee.svg";
+import ChipFood from "../../../public/assets/icons/chip/ChipFood.svg";
 
 export { ChipHeart, ChipPlace, ChipBalloon, ChipCoffee, ChipFood };
