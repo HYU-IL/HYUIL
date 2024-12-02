b6d544ef011533251c8bd7ce929638f7300ec7f6
seo0o519 <seoyoung344@naver.com>
2024-12-02 21:34:55 +0900

    feat #57 - 캠핑, 축제, 레저, 휴양, 지도, 워케이션 페이지 이전버튼 연결

--- src/app/resort/page.tsx
+++ src/app/resort/page.tsx
@@ -15,7 +15,7 @@ export default function ResortPage() {
     <div className="w-full min-h-screen flex flex-col items-center justify-start">
       {/* 상단바 */}
       <div className="grid grid-cols-3 w-full px-[16px] py-3 items-center">
-        <Link href="" className="flex justify-start">
+        <Link href="/main" className="flex justify-start">
           <Image src={left} alt="left" />
         </Link>
         <div className="flex justify-center">
