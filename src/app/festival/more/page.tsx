d5a52bb8f2727ce4b951398cbbbab56042de79c5
seo0o519 <seoyoung344@naver.com>
2024-12-04 00:14:14 +0900

    feat #57 - 워케이션, 축제 페이지 데이터 연동

--- src/app/festival/more/page.tsx
+++ src/app/festival/more/page.tsx
@@ -1,30 +1,26 @@
 import ViewMoreFestivalItem from "@/components/common/ViewMoreFestivalItem";
 import { GrPrevious } from "react-icons/gr";
-
-
-const datas = [
-  // 지역, 축제장소 분리 필요해보이나 일단은 address 하나로 통합해놓음
-  // 페이지 넘어가면서 지역 전달받는 방식 정해진 뒤에 수정 예정
-  {imgurl:"/hotelImage.png", address:"춘천", name:"춘천남이섬축제", startdate:"2024-11-11", enddate:"2024-11-28", liked:false, popular:true, mountain:true, ocean:false},
-]
+import {festivalData} from "@/data/festivalData"; 
+import Link from "next/link";
 
 export default function More() {
+  const festival_data = festivalData
   return (
     <div className="w-full">
         <div className="flex items-center p-[14px] border-b border-[#ededed] sticky top-0 bg-white z-10">
-          <GrPrevious className="w-[20px] h-[20px] text-gray-600" />
+          <Link href="/festival"><GrPrevious className="w-[20px] h-[20px] text-gray-600" /></Link>
           <p className="text-title2 mx-[10px]">휴일이 추천하는 강원도 축제</p>
         </div>
-        {datas.map((data, index) => (
+        {festival_data.map((data, index) => (
             <ViewMoreFestivalItem
             key={index}
-              imgurl={data.imgurl}
-              address={data.address}
+              imgurl={data.image}
+              address={data.rdnm_adr}
               name={data.name}
-              startdate={data.startdate}
-              enddate={data.enddate}
-              liked={data.liked}
-              popular={data.popular}
+              startdate={data.start_date}
+              enddate={data.end_date}
+              liked={false}
+              popular={index%4===0 ? true : false}
               mountain={data.mountain}
               ocean={data.ocean}
 
--- src/app/workation/more/page.tsx
+++ src/app/workation/more/page.tsx
@@ -1,50 +1,47 @@
 import ViewMoreOfficeItem from "@/components/common/ViewMoreOfficeItem";
 import ViewMoreResidenceItem from "@/components/common/ViewMoreResidenceItem";
 import { GrPrevious } from "react-icons/gr";
-
-
-const officedatas = [
-  {imgurl:"/hotelImage.png", address:"강릉", name:"경포그네터", wifi: true, parkinglot: true, time:"8:00~18:00", liked:false, popular:true, mountain:false, ocean:true},
-]
-
-const residencedatas = [
-  {imgurl:"/hotelImage.png", address:"강릉", name:"경포그네터", wifi: true, parkinglot: true, capacity:5, liked:false, popular:true, mountain:false, ocean:true},
-]
+import {workationData} from "@/data/workationData";
+import {residenceData} from "@/data/residenceData";
+import Link from 'next/link';
 
 export default function More() {
+  const workation_data = workationData;
+  const residence_data = residenceData;
+
   return (
     <div className="w-full">
         <div className="flex items-center p-[14px] border-b border-[#ededed] sticky top-0 bg-white z-10">
-          <GrPrevious className="w-[20px] h-[20px] text-gray-600" />
+          <Link href="/workation"><GrPrevious className="w-[20px] h-[20px] text-gray-600" /></Link>
           <p className="text-title2 mx-[10px]">직장인이 꿈꾸는 워케이션</p>
         </div>
-        {officedatas.map((data, index) => (
+        {workation_data.map((data, index) => (
             <ViewMoreOfficeItem
               key={index}
-              imgurl={data.imgurl}
-              address={data.address}
+              imgurl={data.image}
+              address={data.rdnm_adr}
               name={data.name}
-              parkinglot={data.parkinglot}
+              parkinglot={data.parking_lot}
               wifi={data.wifi}
-              time={data.time}
-              liked={data.liked}
-              popular={data.popular}
+              time={data.oper_time}
+              liked={false}
+              popular={index%3==0 ? true : false}
               mountain={data.mountain}
               ocean={data.ocean}
 
             />
         ))}
-        {residencedatas.map((data, index) => (
+        {residence_data.map((data, index) => (
             <ViewMoreResidenceItem
               key={index}
-              imgurl={data.imgurl}
-              address={data.address}
+              imgurl={data.image}
+              address={data.rdnm_adr}
               name={data.name}
-              parkinglot={data.parkinglot}
+              parkinglot={data.parking_lot}
               wifi={data.wifi}
               capacity={data.capacity}
-              liked={data.liked}
-              popular={data.popular}
+              liked={false}
+              popular={index%5==0 ? true : false}
               mountain={data.mountain}
               ocean={data.ocean}
 
